2c168364dbb943b212db754946fca90a6f793855
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-01-11 08:29:07 -0600

    Add files via upload

---
+++ AssassinManager.java
@@ -0,0 +1,39 @@
+public class AssassinManager {
+    public AssassinManager(list<String> names)
+    {
+    	
+    }
+    
+    public String killring()
+    {
+		return null;
+    	
+    }
+    
+    public String graveyard()
+    {
+    	return null;
+    }
+    
+    public boolean killringContains()
+    {
+    	return false;
+    }
+    
+    public boolean graveyeardContains()
+    {
+    	return false;
+    }
+    
+    public boolean isGameOver()
+    {
+    	return false;
+    }
+    
+    public String winner()
+    {
+    	return null;
+    }
+    
+
+}
---
+++ AssassinNode.java
@@ -0,0 +1,23 @@
+ public class AssassinNode 
+ {
+     public final String name;  // this person's name
+     public String killer;      // name of who killed this person (null if alive)
+     public AssassinNode next;  // next node in the list (null if none)
+     
+     /**
+      * Constructs a new node to store the given name and no next node.
+      */
+     public AssassinNode(String name) {
+         this(name, null);
+     }
+
+     /**
+      * Constructs a new node to store the given name and a reference
+      * to the given next node.
+      */
+     public AssassinNode(String name, AssassinNode next) {
+         this.name = name;
+         this.killer = null;
+         this.next = next;
+     }
+ }
