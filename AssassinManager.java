80a0caa4c045c968c2920e885db9e647921bdacd
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-01-13 08:25:53 -0600

    Add files via upload

--- AssassinManager.java
+++ AssassinManager.java
@@ -1,39 +1,61 @@
-public class AssassinManager {
-    public AssassinManager(list<String> names)
-    {
-    	
-    }
-    
-    public String killring()
-    {
-		return null;
-    	
-    }
-    
-    public String graveyard()
-    {
-    	return null;
-    }
-    
-    public boolean killringContains()
-    {
-    	return false;
-    }
-    
-    public boolean graveyeardContains()
-    {
-    	return false;
-    }
-    
-    public boolean isGameOver()
-    {
-    	return false;
-    }
-    
-    public String winner()
-    {
-    	return null;
-    }
-    
+import java.util.List;
 
+public class AssassinManager {
+    // YOUR CODE GOES HERE
+	private AssassinNode frontkill;
+	private AssassinNode frontGrave;
+	
+	public AssassinManager( List<String> names) {
+		for(String i : names) {
+			frontkill = new AssassinNode(i,frontkill);
+		}
+		
+	}
+	public String killRing() {
+		
+		String a="";
+		AssassinNode e = frontkill;
+		while(e.next!=null) {
+			System.out.println(e.name);
+			a+= e.name + " is stalking " + e.next.name + "\n";
+			e = e.next;
+		}
+		a+= e.name + " is stalking " + frontkill.name + "\n";
+		System.out.println(a);
+		return a;
+	}
+	public String graveyard() {
+		String a="";
+		AssassinNode e= frontGrave;
+		 if(e==null) {
+			 return "";
+		 }
+		 while(e.next!=null) {
+			 a+=e.name;
+		 }
+		
+		return "";
+	}
+	public boolean KillRingContains(){
+		return true;
+	}
+	public boolean graveyardContains() {
+		return true;
+	}
+	public boolean isGameOver() {
+		return true;
+	}
+	public String winner() {
+		return "";
+	}
+	public void kill(String name) {
+		
+	}
+	
+	
+		
+	
 }
+
+
+
