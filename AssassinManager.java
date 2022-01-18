70ce1367d10ff403e2558b533cec88d9a74c7515
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-01-18 08:51:53 -0600

    Add files via upload

--- AssassinManager.java
+++ AssassinManager.java
@@ -1,39 +1,5 @@
 public class AssassinManager {
-    public AssassinManager(list<String> names)
-    {
-    	
-    }
-    
-    public String killring()
-    {
-		return null;
-    	
-    }
-    
-    public String graveyard()
-    {
-    	return null;
-    }
-    
-    public boolean killringContains()
-    {
-    	return false;
-    }
-    
-    public boolean graveyeardContains()
-    {
-    	return false;
-    }
-    
-    public boolean isGameOver()
-    {
-    	return false;
-    }
-    
-    public String winner()
-    {
-    	return null;
-    }
-    
+    // YOUR CODE GOES HERE
+
 
 }
