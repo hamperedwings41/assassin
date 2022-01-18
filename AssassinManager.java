fe431339cc9b9da526bd185d71172e85913be877
nuttymannis <51046770+nuttymannis@users.noreply.github.com>
2022-01-18 09:20:24 -0600

    Add files via upload

--- AssassinManager.java
+++ AssassinManager.java
@@ -1,39 +1,108 @@
+
+ import java.util.List;
+
 public class AssassinManager {
-    public AssassinManager(list<String> names)
+    // YOUR CODE GOES HERE
+	private AssassinNode front;
+	private AssassinNode Grave;
+	
+	public AssassinManager( List<String> names) 
+	{
+		for(String x : names) {
+			front = new AssassinNode(x,front);
+		}
+		
+	}
+	public String killRing() {
+		
+		String a = "";
+		AssassinNode f = front;
+		while(f.next!=null) {
+			System.out.println(f.name);
+			a += f.name + " is stalking " + f.next.name + "\n";
+			f = f.next;
+		}
+		a += f.name + " is stalking " + front.name + "\n";
+		System.out.println(a);
+		return a;
+	}
+	
+	public String graveyard() {
+		String  bob = "";
+		AssassinNode b = Grave;
+		 if(b==null) {
+			 return "";
+		 }
+		 while(b.next!=null) {
+			 bob += b.name;
+		 }
+		
+		return "";
+	}
+	
+	public boolean killRingContains(String name)
+	{
+		AssassinNode f = front;
+		while(f != null)
+		{
+			if(f.name == name )
+			{
+				return true;
+			}
+		f = f.next;
+		}
+		return false;
+	}
+	
+	public boolean graveyardContains(String name)
+	{
+		AssassinNode f = Grave;
+		while(f != null)
+		{
+			if(f.name == name )
+			{
+				return true;
+			}
+		f = f.next;
+		}
+		return false;
+	}
+	
+	public boolean isGameOver()
     {
-    	
-    }
-    
-    public String killring()
-    {
-		return null;
-    	
-    }
-    
-    public String graveyard()
-    {
-    	return null;
-    }
-    
-    public boolean killringContains()
-    {
+    	if(front.next == null)
+    	{
+    		return true;
+    	}
     	return false;
     }
-    
-    public boolean graveyeardContains()
-    {
-    	return false;
-    }
-    
-    public boolean isGameOver()
-    {
-    	return false;
-    }
-    
+	
     public String winner()
     {
+    	if(front.next == null)
+    	{
+    		return front.name;
+    	}
     	return null;
     }
     
-
+	public void kill(String name)
+	{	
+		if(Grave == null) {
+			Grave = new AssassinNode(name,null);
+		} else {
+			AssassinNode p = front;
+			while(p.next != null && p.name.toLowerCase().equals(name.toLowerCase())) {
+				p = p.next;
+			}
+			
+			AssassinNode f = Grave;
+			while(f.next != null) {
+				f = f.next;
+			}
+			
+			
+			
+		}
+	}
 }
